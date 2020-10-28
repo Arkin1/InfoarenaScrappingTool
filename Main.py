410c1cc1d51fd14af1c3c529cc3acc944fa0fb5c
bfdyoy <danielbabiceanu@gmail.com>
2020-10-28 11:48:58 +0200

    dataset - arhiva educationala update

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from InfoarenaScrapper import *
 
 isc = InfoarenaScrapper()
 
-# isc.get_problems_links(300)
-# isc.collect_sourcecode_urls("problems.data", "probleme_rezolvate")
+isc.get_problems_links(300)
+isc.collect_sourcecode_urls("problems.data", "probleme_rezolvate")
 isc.collect_urls_arhiva("arhiva_probleme.data")
 isc.collect_sourcecode_urls("arhiva_probleme.data", "arhiva_educationala")
